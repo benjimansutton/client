ab80e74b16a8e1c0e31b18b60e82d346801bbed2
benjimansutton <benjimansutton@gmail.com>
2022-07-27 09:28:33 +0100

    Create the client socket to connect to server via local host

--- src/com/benjiman/main/Main.java
+++ src/com/benjiman/main/Main.java
@@ -1,8 +1,20 @@
 package com.benjiman.main;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        // Try block that connects to the server from this client into the local host 5000
+
+        try(Socket socket = new Socket("localhost", 5000)) {  //127.0.0.100
+
+            // Catch block for the Errors if there are any
+        } catch (IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
+        }
 
 
     }
