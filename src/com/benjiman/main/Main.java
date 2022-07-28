04665f591b66a2efa6c7355d29740cdf4020177b
benjimansutton <benjimansutton@gmail.com>
2022-07-28 07:42:21 +0100

    Server timeouts set up and connected

--- src/com/benjiman/main/Main.java
+++ src/com/benjiman/main/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,9 @@ public class Main {
         // Try block that connects to the server from this client into the local host 6000
 
         try(Socket socket = new Socket("localhost", 6000)) {  //127.0.0.100
+
+            // Timeout for the if the server doesn't respond
+            socket.setSoTimeout(5000);
             BufferedReader echoes = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
             PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
@@ -31,11 +35,15 @@ public class Main {
                 echoString = scanner.nextLine();
 
                 stringToEcho.println(echoString);
-                if(!echoString.equals("exit")) {
+                if (!echoString.equals("exit")) {
                     response = echoes.readLine();
                     System.out.println(response);
                 }
-            } while(!echoString.equals("exit"));
+            } while (!echoString.equals("exit"));
+
+            // This catch block connects with the server in the event of a time out
+        } catch(SocketTimeoutException e) {
+            System.out.println("The socket timed out");
 
 
             // Catch block for the Errors if there are any
